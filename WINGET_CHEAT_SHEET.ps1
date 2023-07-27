c9dcd9b30544b6148daf399b79268f486375bee9
R <107578323+blue-slushy9@users.noreply.github.com>
2023-07-27 10:51:17 -0700

    Add files via upload

---
+++ WINGET_CHEAT_SHEET.ps1
@@ -0,0 +1,26 @@
+# Upgrade all available packages
+winget upgrade --all
+
+# Install a package
+winget install <package>
+
+# Remove a package
+winget uninstall <package>
+
+# Update software package
+winget upgrade <package>
+
+# List installed apps on Windows
+winget list
+
+# Display the package info
+winget show <package>
+
+# Manage repositories
+winget source <options>
+
+# Search for packages in repositories
+winget search <search_string>
+
+# Export the list of installed programs to a file
+winget export
